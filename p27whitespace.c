a233f8df52d8e53573478186d54046fd6e06b1e9
Penelope Herrera <20herrera@stfrancishs.org>
2018-08-31 19:58:22 +0000

    init

---
+++ p27whitespace.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void)
+{
+    float a, b;
+    
+    printf("How much of a bonus did you get? ");
+    scanf(" %f", &a);
+    b = .85 * a;
+    printf("If you give 15 percent to charity, you will still ");
+    printf("have %.2f.\n", b);
+    return 0;
+}
