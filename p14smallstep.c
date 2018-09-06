53311c92867d7961397aad67ad72876a60bce104
Penelope Herrera <20herrera@stfrancishs.org>
2018-09-06 15:42:48 +0000

    init

---
+++ p14smallstep.c
@@ -0,0 +1,8 @@
+// prints out a message on the screen
+#include <stdio.h>
+int main (void)
+{
+    printf("Just one small step for coders. One giant leap for");
+    printf(" programmers!\n");
+    return 0;
+}
